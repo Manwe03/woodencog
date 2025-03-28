c40cb16b0cfc7eeb44e87accbd821038b6b45e3f
Manwe <manu.barrach@icloud.com>
2025-03-28 19:55:47 +0100

    Fix Thirsty Deployer crash #14

---
+++ src/main/java/net/chauvedev/woodencog/mixin/MixinDrinkable.java
@@ -0,0 +1,26 @@
+package net.chauvedev.woodencog.mixin;
+
+import com.simibubi.create.content.kinetics.deployer.DeployerFakePlayer;
+import net.dries007.tfc.util.Drinkable;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = Drinkable.class,remap = false)
+public class MixinDrinkable {
+
+    @Inject(
+            method = "attemptDrink",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static void attemptDrink(Level level, Player player, boolean doDrink, CallbackInfoReturnable<InteractionResult> cir){
+        if(player instanceof DeployerFakePlayer){
+            cir.setReturnValue(InteractionResult.PASS);
+        }
+    }
+}
