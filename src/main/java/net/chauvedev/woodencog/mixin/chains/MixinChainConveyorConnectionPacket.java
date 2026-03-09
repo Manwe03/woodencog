6f7855ea99d630566947549974df55560b23ddff
Manwe <manu.barrach@icloud.com>
2026-03-09 16:28:27 +0100

    #56 Fix chain conveyors dropped items

--- src/main/java/net/chauvedev/woodencog/mixin/chains/MixinChainConveyorConnectionPacket.java
+++ src/main/java/net/chauvedev/woodencog/mixin/chains/MixinChainConveyorConnectionPacket.java
@@ -6,12 +6,17 @@ import net.chauvedev.woodencog.blockEntities.ChainConveyorBlockEntityExtended;
 import net.chauvedev.woodencog.utils.CogUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(value = ChainConveyorConnectionPacket.class, remap = false)
 public class MixinChainConveyorConnectionPacket {
@@ -44,5 +49,32 @@ public class MixinChainConveyorConnectionPacket {
     )
     private void beforeBeAddConnectionTo(ServerPlayer player, ChainConveyorBlockEntity be, CallbackInfo ci) {
         ((ChainConveyorBlockEntityExtended) be).addConnectionToWithChain(targetPos, chain.getItem());
+    }
+
+    //Case: Shift right click chain with Wrench
+    @Inject(
+        method = "applySettings(Lnet/minecraft/server/level/ServerPlayer;Lcom/simibubi/create/content/kinetics/chainConveyor/ChainConveyorBlockEntity;)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/player/Inventory;placeItemBackInInventory(Lnet/minecraft/world/item/ItemStack;)V",
+            ordinal = 0
+        ),
+        locals = LocalCapture.CAPTURE_FAILSOFT
+    )
+    private void beforePlaceItemBackInInventory(ServerPlayer player, ChainConveyorBlockEntity be, CallbackInfo ci, ChainConveyorBlockEntity clbe, int chainCost){
+        ItemLike chain = ((ChainConveyorBlockEntityExtended) be).getConnectionsChain().get(targetPos.subtract(be.getBlockPos()));
+        player.getInventory().placeItemBackInInventory(new ItemStack(chain == null ? Items.CHAIN : chain, Math.min(chainCost, 64)));
+    }
+
+    @Redirect(
+        method = "applySettings(Lnet/minecraft/server/level/ServerPlayer;Lcom/simibubi/create/content/kinetics/chainConveyor/ChainConveyorBlockEntity;)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/player/Inventory;placeItemBackInInventory(Lnet/minecraft/world/item/ItemStack;)V",
+            ordinal = 0
+        )
+    )
+    private void redirectPlaceItemBackInInventory(Inventory instance, ItemStack pStack){
+        //Cancel Default Execution
     }
 }
